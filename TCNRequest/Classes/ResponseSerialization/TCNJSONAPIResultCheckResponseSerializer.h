d8d21bc68069696acb36654ecf42019193693434
周高举 <zgj@shtruecolor.com>
2017-09-28 10:34:29 +0800

    调整 TCNJSONAPIResultCheckResponseSerializer的注释说明

--- TCNRequest/Classes/ResponseSerialization/TCNJSONAPIResultCheckResponseSerializer.h
+++ TCNRequest/Classes/ResponseSerialization/TCNJSONAPIResultCheckResponseSerializer.h
@@ -8,7 +8,7 @@
 
 #import <AFNetworking/AFNetworking.h>
 
-// 返回结果检查失败时抛出错误的Domain
+// 返回的JSON数据检查失败时抛出错误的Domain
 extern NSString * const TCNJSONAPIRRSErrorDomain;
 
 // 出现本文件定义的错误时，调用方仍然可以通过
@@ -17,11 +17,11 @@ extern NSString * const TCNJSONAPIRRSErrorDomain;
 extern NSString * const TCNJSONAPIRRSErrorJSONObjectUserInfoKey;
 
 /**
- 返回结果检查失败的错误类型
+ 返回的JSON数据检查失败的错误类型
 
  - TCNJSONAPIRRSErrorCodeNone: 未定义
- - TCNJSONAPIRRSErrorCodeIsNotDict: 返回结果不是字典
- - TCNJSONAPIRRSErrorCodeStatusIsNotSuccess: 返回结果的status字段不是success
+ - TCNJSONAPIRRSErrorCodeIsNotDict: 返回的JSON数据不是字典
+ - TCNJSONAPIRRSErrorCodeStatusIsNotSuccess: 返回的JSON数据的status字段不是success
  */
 typedef NS_ENUM(NSInteger, TCNJSONAPIRRSErrorCode) {
   TCNJSONAPIRRSErrorCodeNone = 0,
@@ -29,11 +29,13 @@ typedef NS_ENUM(NSInteger, TCNJSONAPIRRSErrorCode) {
   TCNJSONAPIRRSErrorCodeStatusIsNotSuccess = 2
 };
 
-// 自动检查服务器API返回的结果.
+// 自动检查服务器API返回的JSON数据.
 // 如果数据异常会视作请求失败.
+// 注意:这里的”异常“指的是数据结构,字段的异常,并不是指JSON解析异常.
+// 注意:如果返回数据JSON解析出现问题,并不会抛出本文件定义的错误.
 
 // 配合自动切换线路使用,检查失败时会继续切换其他线路.
-// 如果需要在检查失败时停止切换线路执行失败回调,则需要将上面定义的错误加入到TCNAutoDataCenterManager的cceptableErrorType中.
+// 如果需要在检查失败时停止线路切换执行失败回调,则需要将上面定义的错误加入到TCNAutoDataCenterManager的acceptableErrorType中.
 @interface TCNJSONAPIResultCheckResponseSerializer : AFJSONResponseSerializer
 
 @end
